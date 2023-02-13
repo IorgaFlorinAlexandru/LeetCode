a824ee865687268b37f5a4db6a78199b90eb9432
Iorga Florin <iorgaflorinalexandru@gmail.com>
2023-02-13 14:20:37 +0200

    Resolved 605

--- LeetCode/605/CanPlaceFlowers.cs
+++ LeetCode/605/CanPlaceFlowers.cs
@@ -4,9 +4,9 @@
 	{
 		public static void Execute()
 		{
-			int[] flowerbed = { 1, 0, 0, 0, 0, 1 };
+			int[] flowerbed = { 1, 0, 0, 0 };
 
-			Console.WriteLine(CanPlaceFlowers(flowerbed, 2));
+			Console.WriteLine(CanPlaceFlowers(flowerbed, 1));
 
 		}
 
@@ -14,17 +14,43 @@
 		{
 			int count = 0;
 
-			for(int i = 0; i < flowerbed.Length - 1; i++)
+			int index = 1;
+
+			if (flowerbed.Length > 1)
+			{
+				if (flowerbed[0] == 0 && flowerbed[1] == 0)
+				{
+					flowerbed[0] = 1;
+					count++;
+					index = 2;
+				}
+			}
+			else
+				count = flowerbed[0] == 0 ? 1 : 0;
+
+			while(index < flowerbed.Length - 1)
+			{
+                if (flowerbed[index] == 0 && flowerbed[index - 1] == 0 && flowerbed[index + 1] == 0)
+                {
+					flowerbed[index] = 1;
+                    count++;
+                    index++;
+                }
+
+                index++;
+			}
+
+			if(flowerbed.Length > 1 && index < flowerbed.Length)
 			{
 
-				if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+				if (flowerbed[index] == 0 && flowerbed[index-1] == 0 && flowerbed.Length > 1)
 				{
 					count++;
-					i++;
 				}
 			}
 
-			return count == n;
+
+			return count >= n;
 		}
 
 
